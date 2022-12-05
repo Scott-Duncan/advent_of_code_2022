f0c93f3611b0ea7240ab7469df41956b8a4260fb
Scott Duncan <scott.duncan.work@gmail.com>
2022-12-05 22:26:33 +0000

    Finished part 2

--- dec_5/src/crane.cpp
+++ dec_5/src/crane.cpp
@@ -19,12 +19,17 @@ Crane::Crane(const std::string &file_location, const int columns) {
   getline(myfile, line);
 
   while (getline(myfile, line)) {
+    if (line.empty()) {
+      break;
+    }
     std::vector<int> instructions = getMoves(line);
     useCrane(instructions.at(1), instructions.at(2), instructions.at(0));
   }
 
   for (int z = 0; z < 9; z++) {
-    std::cout << stack.at(z).at(stack.at(z).size()-1);
+    if (stack.at(z).size() != 0) {
+      std::cout << stack.at(z).at(stack.at(z).size() - 1);
+    }
     std::cout << std::endl;
   }
 }
@@ -56,16 +61,24 @@ void Crane::populateStack(const std::string &row, const int columns,
 
 void Crane::useCrane(const int start_col, const int end_col,
                      int number_of_moves) {
-  for (int i = 0; i < number_of_moves; i++) {
-    if (stack.at(start_col - 1).size() != 0) {
-      auto size = stack.at(start_col - 1).size();
-      stack.at(end_col - 1).push_back(stack.at(start_col - 1).at(size - 1));
+  auto moves{0};
+  std::cout << "Start col size: " << stack.at(start_col - 1).size()
+            << ", End col size: " << stack.at(end_col - 1).size()
+            << " number of moves: " << number_of_moves << std::endl;
+  int start = (int(stack.at(start_col - 1).size()) - number_of_moves);
+  std::cout << start << std::endl;
+  for (int i = start; i < int(stack.at(start_col - 1).size()); i++) {
+    std::cout << i;
+    if (i >= 0 && i < int(stack.at(start_col - 1).size())) {
+      stack.at(end_col - 1).push_back(stack.at(start_col - 1).at(i));
+      moves++;
+    }
+  }
+  std::cout << std::endl;
+  for (int j = 0; j < moves; j++) {
+    if (stack.at(start_col - 1).size() > 0) {
       stack.at(start_col - 1).pop_back();
       stack.at(start_col - 1).shrink_to_fit();
-      std::cout << "Size of " << start_col - 1 << size -1 << std::endl;
-    }else {
-      std::cout << "empty" << std::endl;
-      break;
     }
   }
 }
