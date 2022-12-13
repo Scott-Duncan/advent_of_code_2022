4312d105d8acb1db7bc26c2d89fb47cf6cf35481
Scott Duncan <scott.duncan.work@gmail.com>
2022-12-13 21:23:17 +0000

    Second part, 1 and two with help.

--- dec_13/src/main.cpp
+++ dec_13/src/main.cpp
@@ -1,10 +1,23 @@
-#include <string>
+#include <algorithm>
 #include <fstream>
 #include <iostream>
-#include <boost/algorithm/string.hpp>
+#include <string>
+#include <vector>
 
 bool is_number (const char c) {
   return c >= '0' && c <= '9';
+}
+
+void debug_print(const std::string& packet1, const std::string& packet2, const int i1, const int i2) {
+  std::cout << packet1 << ' ' << packet2 << '\n';
+  for (int i = 0; i < i1; i++) std::cout << ' ';
+  std::cout << '^';
+  for (int i = i1 + 1; i < packet1.size(); i++) std::cout << ' ';
+  std::cout << ' ';
+  for (int i = 0; i < i2; i++) std::cout << ' ';
+  std::cout << '^';
+  for (int i = i2 + 1; i < packet2.size(); i++) std::cout << ' ';
+  std::cout << '\n';
 }
 
 bool compare (const std::string& packet1, const std::string& packet2) {
@@ -23,9 +36,13 @@ bool compare (const std::string& packet1, const std::string& packet2) {
         i2++;
       }
       if (n1 == n2) {
+        // debug_print(packet1, packet2, i1, i2);
+        // std::cout << "Equal" << '\n';
         continue;
       }
       if (n1 < n2) {
+        // debug_print(packet1, packet2, i1, i2);
+        // std::cout << "Correct order" << '\n';
         return true;
       }
       break;
@@ -33,6 +50,8 @@ bool compare (const std::string& packet1, const std::string& packet2) {
       i1++;
       i2++;
     } else if (packet1[i1] == ']') {
+      // debug_print(packet1, packet2, i1, i2);
+      // std::cout << "Correct order" << '\n';
       return true;
     } else if (packet2[i2] == ']') {
       break;
@@ -43,10 +62,15 @@ bool compare (const std::string& packet1, const std::string& packet2) {
     }
   }
   if (i1 == packet1.size()) {
+    // debug_print(packet1, packet2, i1, i2);
+    // std::cout << "Correct order" << '\n';
     return true;
   }
+  // debug_print(packet1, packet2, i1, i2);
+  // std::cout << "Wrong order" << '\n';
   return false;
 }
+
 
 int main(int argc, char * argv[]) {
   std::string input;
@@ -54,21 +78,30 @@ int main(int argc, char * argv[]) {
     input = argv[1];
   }
 
-  std::string packet1, packet2;
+  std::string line;
   std::fstream file(input);
-
-  int index = 0;
-  int sum = 0;
-
-  while(std::getline(file, packet1)) {
-    if (packet1.empty()) continue;
-    std::getline(file, packet2);
-    index++;
-    if (compare(packet1, packet2)) {
-      sum += index;
-    }
+  std::vector<std::string> packets;
+  while(std::getline(file, line)) {
+    if (line.empty()) continue;
+    packets.push_back(line);
   }
 
-  std::cout << sum << '\n';
+  const std::vector<std::string> divider_packets {
+    "[[2]]",
+    "[[6]]"
+  };
+
+  for (const auto& divider_packet : divider_packets) {
+    packets.push_back(divider_packet);
+  }
+
+  std::sort(std::begin(packets), std::end(packets), compare);
+
+  int ans = 1;
+  for (const auto& divider_packet : divider_packets) {
+    ans *= (std::distance(std::begin(packets), std::find(std::begin(packets), std::end(packets), divider_packet)) + 1);
+  }
+
+  std::cout << ans << '\n';
   return 0;
 }
